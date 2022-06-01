d54826db3d92857d22dc38f0b4551954853a8a30
ishit16 <ishitgarg1231@gmail.com>
2022-06-01 18:03:49 +0530

    Auth Button Added

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,25 +1,36 @@
+import { Button } from "antd";
+import { Helmet } from "react-helmet";
+import { AuthButton } from "../components/buttons/Authentication";
 function LandingPage() {
   return (
-    <div className="min-h-screen flex flex-col text-white">
-      <main className="container mx-auto px-6 pt-16 flex-1 text-center">
-        <h2 className="font-sans text-2xl md:text-3xl lg:text-4xl uppercase">
-          Welcome to
-        </h2>
-        <h1 className="font-sans text-3xl md:text-4xl lg:text-6xl font-black uppercase mb-8">
-          Seasons of Code Portal
-        </h1>
+    <>
+      <Helmet>
+        <title>Login - Seasons of Code</title>
+      </Helmet>
+      <div className="min-h-screen flex flex-col text-white">
+        <main className="container mx-auto px-6 pt-16 flex-1 text-center">
+          <h2 className="font-sans text-2xl md:text-3xl lg:text-4xl uppercase">
+            Welcome to
+          </h2>
+          <h1 className="font-sans text-3xl md:text-4xl lg:text-6xl font-extrabold uppercase mb-8">
+            Seasons of Code Portal
+          </h1>
 
-        <div className="text-lg md:text-2xl lg:text-3xl py-2 px-2 md:py-2 md:px-2 lg:py-2 lg:px-2 bg-white bg-opacity-10 w-fit mx-auto my-auto rounded-full">
-          <img
-            className="object-scale-down py-2 h-12 w-48"
-            src="src/pages/images/logo.png"
-          />
-        </div>
-      </main>
-      <footer className="container mx-auto p-6 flex flex-col md: flex-row items-center justify-between">
-        <p className="font-sans">Built with {`\u2764`} by Ishit Garg</p>
-      </footer>
-    </div>
+          <div className="text-lg md:text-2xl lg:text-3xl py-2 px-2 md:py-2 md:px-2 lg:py-2 lg:px-2 bg-white bg-opacity-10 w-fit mx-auto my-auto rounded-full">
+            <img
+              className="object-scale-down py-2 h-12 w-48"
+              src="src/pages/images/logo.png"
+            />
+          </div>
+          <button class="mt-16 text-lg w-96 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border-none">
+            Login with SSO
+          </button>
+        </main>
+        <footer className="text-lg container mx-auto p-6 flex flex-col md: flex-row items-center justify-between">
+          <p className="font-sans">Built with {`\u2764`} by Ishit Garg</p>
+        </footer>
+      </div>
+    </>
   );
 }
 
---
+++ src/styles/fontSize.jsx
@@ -0,0 +1,55 @@
+// ? https://tailwindcss.com/docs/font-size
+// * unit: rem
+// * '$' added as variable cannot start with a number
+const size = {
+  xs: 0.75,
+  sm: 0.875,
+  md: 1.0,
+  lg: 1.125,
+  xl: 1.25,
+  $2xl: 1.5,
+  $3xl: 1.875,
+  $4xl: 2.25,
+  $5xl: 3,
+  $6xl: 3.75,
+  $7xl: 4.5,
+};
+
+// ? mobile first (font-size increases with window width)
+// * fluid transition between mobile and desktop fonts
+// * https://css-tricks.com/linearly-scale-font-size-with-css-clamp-based-on-the-viewport/
+const makeResponsive = (fontSizeSmall, fontSizeLarge) =>
+  `clamp(${fontSizeSmall}rem, ${2 * fontSizeSmall}vw, ${fontSizeLarge}rem)`;
+
+const fontSize = {
+  static: {
+    xs: `${size.xs}rem`,
+    sm: `${size.sm}rem`,
+    md: `${size.md}rem`,
+    lg: `${size.lg}rem`,
+    xl: `${size.xl}rem`,
+    $2xl: `${size.$2xl}rem`,
+    $3xl: `${size.$3xl}rem`,
+    $4xl: `${size.$4xl}rem`,
+    $5xl: `${size.$5xl}rem`,
+    $6xl: `${size.$6xl}rem`,
+  },
+
+  responsive: {
+    xs: makeResponsive(size.xs, size.sm),
+    sm: makeResponsive(size.sm, size.md),
+    md: makeResponsive(size.md, size.lg),
+    lg: makeResponsive(size.lg, size.xl),
+    xl: makeResponsive(size.xl, size.$2xl),
+    $2xl: makeResponsive(size.$2xl, size.$3xl),
+    $3xl: makeResponsive(size.$3xl, size.$4xl),
+    $4xl: makeResponsive(size.$4xl, size.$5xl),
+    $5xl: makeResponsive(size.$5xl, size.$6xl),
+    $6xl: makeResponsive(size.$6xl, size.$7xl),
+  },
+};
+
+export const fontSizeS = fontSize.static;
+export const fontSizeR = fontSize.responsive;
+
+export default fontSize;
