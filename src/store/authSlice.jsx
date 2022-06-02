715231cdbb16f03affaa9e6ef8ca830976050aab
ishit16 <ishitgarg1231@gmail.com>
2022-06-02 23:13:57 +0530

    Slice for Auth::Store

---
+++ src/store/authSlice.jsx
@@ -0,0 +1,60 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API } from "../api/api";
+
+const initialState = {
+  isAuthenticated: null,
+  loading: false,
+  profile: {},
+};
+
+export const loginAction = createAsyncThunk("auth/loginAction", API.auth.login);
+
+export const logoutAction = createAsyncThunk(
+  "auth/getAuthStatusAction",
+  API.auth.authenticate
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: {
+    [loginAction.fulfilled]: (state, { payload }) => {
+      state.isAuthenticated = true;
+      state.loading = false;
+    },
+    [loginAction.pending]: (state) => {
+      state.loading = true;
+    },
+    [loginAction.rejected]: (state) => {
+      state.loading = false;
+    },
+
+    [logoutAction.fulfilled]: (state, action) => {
+      state.isAuthenticated = false;
+      state.loading = false;
+    },
+    [logoutAction.pending]: (state) => {
+      state.loading = true;
+    },
+    [logoutAction.rejected]: (state) => {
+      state.loading = false;
+    },
+
+    [getAuthStatusAction.fulfilled]: (state, { payload }) => {
+      state.isAuthenticated = true;
+      state.loading = false;
+    },
+    [getAuthStatusAction.pending]: (state) => {
+      state.loading = true;
+    },
+    [getAuthStatusAction.rejected]: (state) => {
+      state.isAuthenticated = false;
+      state.loading = false;
+    },
+  },
+});
+
+export const selectAuthLoading = (state) => state.auth.loading;
+export const selectIsAuthenticated = (state) => state.auth.isAuthenticated;
+
+export default authSlice.reducer;
