617c5a1f71285b981d278a442006382af6731afd
ishit16 <ishitgarg1231@gmail.com>
2022-06-02 18:34:04 +0530

    Landing Page Complete

--- src/components/buttons/Authentication.jsx
+++ src/components/buttons/Authentication.jsx
@@ -1,21 +1,6 @@
-import { Button } from "antd";
-import { lighten } from "polished";
-import styled from "styled-components";
+import {button} from ant
 
-export const AuthButton = styled(Button)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: 500;
-  border-radius: 0.5rem;
-  ${({ color }) =>
-    color &&
-    `
-  background: ${color};
-  border-color: ${color};
-  &:hover {
-    background: ${lighten(0.1, color)};
-    border-color: ${lighten(0.1, color)};
-  }
-    `}
-`;
+function AuthButton()
+{
+
+}
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,12 +1,15 @@
 import { Button } from "antd";
 import { Helmet } from "react-helmet";
-import { AuthButton } from "../components/buttons/Authentication";
+import "./auth.css";
+// import { ReactComponent as ResoBinLogo } from "../assets/logo.svg";
+
 function LandingPage() {
   return (
     <>
       <Helmet>
         <title>Login - Seasons of Code</title>
       </Helmet>
+
       <div className="min-h-screen flex flex-col text-white">
         <main className="container mx-auto px-6 pt-16 flex-1 text-center">
           <h2 className="font-sans text-2xl md:text-3xl lg:text-4xl uppercase">
@@ -22,12 +25,26 @@ function LandingPage() {
               src="src/pages/images/logo.png"
             />
           </div>
-          <button class="mt-16 text-lg w-96 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border-none">
-            Login with SSO
-          </button>
+          <div className="auth-container">
+            <h1 className="font-sans">Join the Force</h1>
+            {/* <ResoBinLogo width="3rem" alt="logo" /> */}
+            <div className="py-2 px-2 md:py-2 md:px-2 lg:py-2 lg:px-2 w-fit mx-auto my-auto">
+              <button
+                type="button"
+                size="large"
+                class="opacity-100 border-none focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+              >
+                <span className="text-white font-sans font-bold text-lg">
+                  Login with SSO
+                </span>
+              </button>
+            </div>
+          </div>
         </main>
         <footer className="text-lg container mx-auto p-6 flex flex-col md: flex-row items-center justify-between">
-          <p className="font-sans">Built with {`\u2764`} by Ishit Garg</p>
+          <p className="font-bold font-sans">
+            Built with {`\u2764`} by Ishit Garg
+          </p>
         </footer>
       </div>
     </>
