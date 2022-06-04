0694598c3c007caa9e4e7422351cd6fc635fd346
ishit16 <ishitgarg1231@gmail.com>
2022-06-04 22:52:25 +0530

    Resolved a bug on landing page

---
+++ src/store/index.jsx
@@ -0,0 +1,24 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+import { persistStore } from "redux-persist";
+
+import { persistedReducer } from "./combineReducers";
+
+const middleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
+
+const logger = createLogger({
+  collapsed: (getState, action, logEntry) => !logEntry.error,
+});
+
+if (import.meta.env.NODE_ENV === "development") middleware.push(logger);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV === "development",
+  middleware,
+  preloadedState: {},
+});
+
+export const persistor = persistStore(store);
