8c9faae870dd4758bfc24c6863c1ecefcffb4332
ishit16 <ishitgarg1231@gmail.com>
2022-06-07 17:27:33 +0530

    Fonts Improved

--- src/components/home/ChooseRole.jsx
+++ src/components/home/ChooseRole.jsx
@@ -7,7 +7,7 @@ function ChooseRoll() {
       <div className="mt-16 md:mt-32  w-2/3 home-color rounded-xl mx-auto">
         <section className="wrapper">
           <header className="text-center p-2">
-            <h2 className="pt-4 text-3xl text-white font-sans">
+            <h2 className="pt-4 text-2xl md:text-4xl text-white font-heading-home">
               Join the Force
             </h2>
             <p className="text-white font-sans">
@@ -19,8 +19,10 @@ function ChooseRoll() {
               <div className=" grid col-span-6 md:col-span-12 text-center p-3 mx-auto">
                 <section>
                   <img className="major-soc" src="src\assets\light-siber.svg" />
-                  <h3 className="text-white font-sans text-2xl">Master</h3>
-                  <p className="text-white font-sans">
+                  <h3 className="text-white font-sans text-lg md:text-3xl font-heading-home">
+                    Master
+                  </h3>
+                  <p className="text-white font-sans md:text-lg font-para-home">
                     I can feel you code. <br /> It gives you focus. It makes you
                     stronger. <br /> Use the Force <br /> and someday you will
                     be the most powerful Jedi ever.
@@ -31,7 +33,7 @@ function ChooseRoll() {
                       size="large"
                       className="opacity-100 border-none focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
                     >
-                      <span className="text-white font-sans font-bold text-lg">
+                      <span className="text-white font-sans font-bold text-lg md:text-2xl">
                         Master
                       </span>
                     </button>
@@ -45,8 +47,10 @@ function ChooseRoll() {
                     className="major-soc"
                     src="src\assets\light-siber-one.svg"
                   />
-                  <h3 className="text-white font-sans text-2xl">Padawan</h3>
-                  <p className="text-white font-sans">
+                  <h3 className="text-white font-sans text-lg md:text-3xl font-heading-home">
+                    Padawan
+                  </h3>
+                  <p className="text-white font-sans md:text-lg font-para-home">
                     The Force is strong with you. <br />
                     Train yourself to let go of everything you fear to loose.{" "}
                     <br /> The Force will be with you always. <br /> Ready are
@@ -58,7 +62,7 @@ function ChooseRoll() {
                       size="large"
                       className="opacity-100 border-none focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
                     >
-                      <span className="text-white font-sans font-bold text-lg">
+                      <span className="text-white font-sans font-bold text-lg md:text-2xl">
                         Padawan
                       </span>
                     </button>
