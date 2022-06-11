69d9278ae8d8b24fd478ac99f39e0a1385afa5ad
ishit16 <ishitgarg1231@gmail.com>
2022-06-11 19:29:47 +0530

    All projects component started

---
+++ src/pages/allProjects.jsx
@@ -0,0 +1,12 @@
+function ApprovedProjects() {
+  return (
+    <>
+      <div>
+        <p>All Projects</p>
+        <p>Yess bitch my project got approved by WnCC Managers</p>
+      </div>
+    </>
+  );
+}
+
+export default ApprovedProjects;
