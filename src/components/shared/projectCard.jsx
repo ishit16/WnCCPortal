1f4e966c8947706cea869bfd9db54566e0c61c4f
ishit16 <ishitgarg1231@gmail.com>
2022-06-12 18:39:05 +0530

    Improved Styling

--- src/components/shared/projectCard.jsx
+++ src/components/shared/projectCard.jsx
@@ -6,10 +6,12 @@ function ProjectCard() {
     <div>
       <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52 project-card">
         <img src="src/assets/project1.png" alt="project1" className="h-40" />
-        <h2 className="text-center px-2">Dummy Project SOC</h2>
+        <h2 className="text-center font-project-heading px-2">
+          Dummy Project SOC
+        </h2>
         <Link
           to={"/"}
-          className=" font-sans no-underline text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium text-center p-3"
+          className="text-lg  font-para-home no-underline  text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium text-center p-3"
         >
           Know More
         </Link>
--- src/pages/allProjects.jsx
+++ src/pages/allProjects.jsx
@@ -4,9 +4,8 @@ function ApprovedProjects() {
   return (
     <div>
       <div className="md:ml-36">
-        <h1 className="font-bold font-sans text-white text-xl text-center md:text-3xl md:mt-12 mb-4">
-          Check out the <span className="text-red-500">Projects</span> of this
-          year
+        <h1 className="font-bold font-heading-home text-white text-xl text-center md:text-4xl md:mt-12 mb-4">
+          Endeavours of the <span className="text-red-500">Jedi</span> Order
         </h1>
         <div className="flex flex-wrap justify-center md:all-projects">
           <ProjectCard />
