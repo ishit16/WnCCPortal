23d83d398866848b900eff2743f398770c99080d
ishit16 <ishitgarg1231@gmail.com>
2022-06-13 19:29:18 +0530

    Responsiveness improved

--- src/pages/allProjects.jsx
+++ src/pages/allProjects.jsx
@@ -3,11 +3,11 @@ import "./auth.css";
 function ApprovedProjects() {
   return (
     <div>
-      <div className="md:ml-36">
-        <h1 className="font-bold font-heading-home text-white text-xl text-center md:text-4xl md:mt-12 mb-4">
+      <div className="lg:ml-36">
+        <h1 className="font-bold font-heading-home text-white text-xl text-center mx-auto md:text-4xl md:mt-12 mb-4">
           Endeavours of the <span className="text-red-500">Jedi</span> Order
         </h1>
-        <div className="flex flex-wrap justify-center md:all-projects">
+        <div className="flex flex-wrap justify-center md:all-projects ">
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
