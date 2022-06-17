3e3dba8773c50f59bb7bd737dc4fcf3ee13be7f8
ishit16 <ishitgarg1231@gmail.com>
2022-06-17 18:27:24 +0530

    Cleaned Home for performance

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,8 @@
-import { useEffect, useState } from "react";
 import Helmet from "react-helmet";
-import { useDispatch, useSelector } from "react-redux";
 import ChooseRoll from "../components/home/ChooseRole";
-import { selectIsAuthenticated } from "../store/authSlice";
-import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import localStorage from "redux-persist/es/storage";
 
 function Home() {
-  const [visible, setVisible] = useState(true);
-  const { isAuthenticated, loading } = useSelector((state) => state.auth);
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("Hello!");
-  }, []);
-  const notify = () => {};
-
   return (
     <>
       <Helmet>
